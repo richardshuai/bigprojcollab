df3e417199aaba4bb4d62aae298688424383fe6d
Richard Shuai <richardshuai@berkeley.edu>
2019-03-30 12:16:27 -0700

    Testing pretty-quick

--- server.js
+++ server.js
@@ -1,45 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const server = app.listen(process.env.PORT || 8080);
-let io = require('socket.io')(server);
+let io = require("socket.io")(server);
 let doc = {
-  state: ''
+  state: ""
 };
 let numClients = 0;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/ping', function (req, res) {
-  return res.send('pong');
+app.get("/ping", function(req, res) {
+  return res.send("pong");
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-console.log('Server is now listening on port 8080.');
+console.log("Server is now listening on port 8080.");
 
-io.on('connection', connection);
+io.on("connection", connection);
 
 function connection(client) {
-  console.log('A new user with id ' + client.id + ' has entered.');
+  console.log("A new user with id " + client.id + " has entered.");
   numClients++;
 
-
   if (doc.state) {
-    client.emit('newUser', doc.state);
+    client.emit("newUser", doc.state);
   }
-  
-  client.on('userEdit', handleTextSent);
+
+  client.on("userEdit", handleTextSent);
 
   function handleTextSent(content) {
-    client.broadcast.emit('receiveEdit', content);
+    client.broadcast.emit("receiveEdit", content);
     doc.state = content;
   }
 
-  client.on('disconnect', function () {
-    console.log('Client with id ' + client.id + ' has disconnected.');
+  client.on("disconnect", function() {
+    console.log("Client with id " + client.id + " has disconnected.");
     numClients--;
   });
 }
--- src/App.js
+++ src/App.js
@@ -1,46 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Editor } from 'slate-react'
-import { Value } from 'slate'
-import socketIOClient from 'socket.io-client';
+import React, { Component } from "react";
+import "./App.css";
+import { Editor } from "slate-react";
+import { Value } from "slate";
+import socketIOClient from "socket.io-client";
 
 const initialValue = Value.fromJSON({
   document: {
     nodes: [
       {
-        object: 'block',
-        type: 'paragraph',
+        object: "block",
+        type: "paragraph",
         nodes: [
           {
-            object: 'text',
+            object: "text",
             leaves: [
               {
-                text: 'A line of text in a paragraph.',
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-})
+                text: "A line of text in a paragraph."
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+});
 
 class App extends Component {
-
   state = {
     value: initialValue,
-    socket: 'uninitialized',
+    socket: "uninitialized",
     response: false
   };
 
   componentDidMount() {
     const socket = socketIOClient();
-    socket.on('newUser', (existingContent) => {
+    socket.on("newUser", existingContent => {
       const updateValue = JSON.parse(existingContent);
-      this.setState({ value: Value.fromJSON(updateValue)}); 
+      this.setState({ value: Value.fromJSON(updateValue) });
     });
-    socket.on('receiveEdit', (newContent) => {
-      this.setState({ value: Value.fromJSON(JSON.parse(newContent))}); 
+    socket.on("receiveEdit", newContent => {
+      this.setState({ value: Value.fromJSON(JSON.parse(newContent)) });
     });
     this.setState({ socket: socket, response: true });
   }
@@ -52,29 +51,28 @@ class App extends Component {
     socket.off("receiveEdit");
   }
   */
-  
+
   // On change, update the app's React state with the new editor value.
   onChange = ({ value }) => {
     if (value.document !== this.state.value.document) {
       this.setState({ value: value }, () => {
-        this.state.socket.emit('userEdit', JSON.stringify(value.toJSON()));
+        this.state.socket.emit("userEdit", JSON.stringify(value.toJSON()));
       });
     }
-  }
+  };
 
-  
   // Render the editor.
   render() {
     if (!this.state.response) {
-      return 'Loading sockets...'
+      return "Loading sockets...";
     }
     return (
-    <div className="App">
-      <h3>Hello! Slate Text Editor</h3>
-      <div className="Editor">
-        <Editor value={this.state.value} onChange={this.onChange} />
+      <div className="App">
+        <h3>Hello! Slate Text Editor</h3>
+        <div className="Editor">
+          <Editor value={this.state.value} onChange={this.onChange} />
+        </div>
       </div>
-    </div>
     );
   }
 }
