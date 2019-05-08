8ecfd39780c4dd008def9212f95fbe0a4028c38f
Richard Shuai <richardshuai@berkeley.edu>
2019-05-07 19:02:37 -0700

    Fixed cursor edge cases for inline comments?

--- src/components/Editor/OnChange.js
+++ src/components/Editor/OnChange.js
@@ -1,9 +1,91 @@
 import { app } from "../../App";
 import { Document, Range } from "slate";
 
-export const onChange = change => {
+export const onChange = async change => {
   // app.setState({ value: event.value }, () => {
   //   app.state.socket.emit("userEdit", JSON.stringify(event.value.toJSON()));
   // });
   app.setState({ value: change.value });
+  // console.log(JSON.stringify(change.operations, null, 2));
+  // console.log(JSON.stringify(change.value, null, 2));
+
+  const selectionOps = change.operations.filter(
+    operation => operation.type === "set_selection"
+  );
+  console.log(JSON.stringify(selectionOps, null, 2));
+  // console.log("text" + JSON.stringify(app.editor.value.focusText));
+
+  if (selectionOps.size >= 1) {
+    handleSelectionChange(selectionOps);
+  }
 };
+
+const handleSelectionChange = async selectionOps => {
+  const { editor } = app;
+  const { value } = editor;
+
+  /* Only look at the focus when detecting if in comment */
+  const currFocus = value.selection.focus;
+  const collapsedToFocus = Range.create({
+    anchor: currFocus,
+    focus: currFocus
+  });
+
+  const isFocusNegative = selectionOps.some(op => {
+    if (!op.get("properties").focus) {
+      return false;
+    }
+    return op.get("properties").focus.offset === -1;
+  });
+
+  /* Case 2 */
+  if (isFocusNegative) {
+    await app.editor.moveBackward(1);
+  }
+  // console.log("currFocus information");
+  // console.log(JSON.stringify(currFocus, null, 2));
+
+  /* There should only ever be one bottom-most leaf inline in a collapsed range */
+  const leafInlines = value.document.getLeafInlinesAtRange(collapsedToFocus);
+  if (leafInlines.size === 1) {
+    const commentNode = getLeafComment(leafInlines.first());
+    // console.log("1 leaf inline detected.");
+    /* Change to trigger only when moving right. This is to prevent case #1 */
+    if (currFocus.offset === 0) {
+      // console.log(value.focusText.text);
+      if (value.focusText.text === "") {
+        console.log("Case 1");
+        await app.editor.moveForward(1);
+      } else if (currFocus.offset === -1) {
+        // await app.editor.moveForward(1);
+      }
+    } else if (currFocus.offset === -1) {
+      // if (value.focusText.text === "") {
+      //   await app.editor.moveBackward(1);
+      // }
+    }
+    if (commentNode) {
+      console.log(
+        "Bottommost key should be: " + commentNode.data.get("uniqueKey")
+      );
+      // app.expandCommentFromInline(commentNode);
+    }
+  } else if (leafInlines.size > 1) {
+    throw RangeError("More than one leafInline in collapsed range.");
+  } else {
+    // console.log("no leaf inline");
+    // app.unexpandCommentFromInline();
+  }
+};
+
+const getLeafComment = node => {
+  while (node && !Document.isDocument(node)) {
+    if (node.type === "comment") {
+      return node;
+    }
+    node = node.getParent(node.key);
+  }
+  return;
+};
+
+const getParentComment = node => {};
--- src/components/Editor/OnSelectionChange.js
+++ src/components/Editor/OnSelectionChange.js
@@ -2,47 +2,45 @@
 import { app } from "../../App";
 import { Document, Range } from "slate";
 export const onSelectionChange = (event, editor) => {
-  if (editor.value.selection.isBlurred) {
-    return;
-  }
-  console.log("onselect");
-  const { value } = editor;
-  /* Only look at the focus when detecting if in comment */
-  const currFocus = value.selection.focus;
-  const collapsedToFocus = Range.create({
-    anchor: currFocus,
-    focus: currFocus
-  });
-
-  /* There should only ever be one bottom-most leaf inline in a collapsed range */
-  const leafInlines = value.document.getLeafInlinesAtRange(collapsedToFocus);
-  if (leafInlines.size === 1) {
-    const commentNode = getLeafComment(leafInlines.first());
-    if (commentNode) {
-      // console.log("Leaf inline detected?");
-      // console.log(
-      //   "Bottommost key should be: " + commentNode.data.get("uniqueKey")
-      // );
-      // app.expandCommentFromInline(commentNode);
-      event.preventDefault();
-    }
-  } else if (leafInlines.size > 1) {
-    throw RangeError("More than one leafInline in collapsed range.");
-  } else {
-    event.preventDefault();
-    // console.log("no leaf inline");
-    // app.unexpandCommentFromInline();
-  }
+  // if (editor.value.selection.isBlurred) {
+  //   return;
+  // }
+  // console.log("onselect");
+  //   const { value } = editor;
+  //   /* Only look at the focus when detecting if in comment */
+  //   const currFocus = value.selection.focus;
+  //   const collapsedToFocus = Range.create({
+  //     anchor: currFocus,
+  //     focus: currFocus
+  //   });
+  //   /* There should only ever be one bottom-most leaf inline in a collapsed range */
+  //   const leafInlines = value.document.getLeafInlinesAtRange(collapsedToFocus);
+  //   if (leafInlines.size === 1) {
+  //     const commentNode = getLeafComment(leafInlines.first());
+  //     if (commentNode) {
+  //       // console.log("Leaf inline detected?");
+  //       // console.log(
+  //       //   "Bottommost key should be: " + commentNode.data.get("uniqueKey")
+  //       // );
+  //       // app.expandCommentFromInline(commentNode);
+  //       event.preventDefault();
+  //     }
+  //   } else if (leafInlines.size > 1) {
+  //     throw RangeError("More than one leafInline in collapsed range.");
+  //   } else {
+  //     event.preventDefault();
+  //     // console.log("no leaf inline");
+  //     // app.unexpandCommentFromInline();
+  //   }
+  // };
+  // const getLeafComment = node => {
+  //   while (node && !Document.isDocument(node)) {
+  //     if (node.type === "comment") {
+  //       return node;
+  //     }
+  //     node = node.getParent(node.key);
+  //   }
+  //   return;
 };
 
-const getLeafComment = node => {
-  while (node && !Document.isDocument(node)) {
-    if (node.type === "comment") {
-      return node;
-    }
-    node = node.getParent(node.key);
-  }
-  return;
-};
-
-const getParentComment = node => {};
+// const getParentComment = node => {};
